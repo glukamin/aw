c48a63317463b1f33bb84e6953baf9fd4f08a20d
CRaZY <cr4zylo@yahoo.com>
2021-10-24 00:30:45 +0200

    Delete test - Copy1.sql

--- asd/test - Copy1.sql
+++
@@ -1,9 +0,0 @@
-select * from dbo.EmployeeSchedule where CreatedBy = 'MDJ'
-order by EffectiveDate
-
-select es.ID, t.ID, t.StartDate, es.EffectiveDate from dbo.Timesheet t
-inner join dbo.EmployeeSchedule es
-on t.EmployeeScheduleId = es.ID
-where t.CreatedBy = 'MDJ' and es.ID = 247673
-
-delete from dbo.EmployeeSchedule where ID = 247673
